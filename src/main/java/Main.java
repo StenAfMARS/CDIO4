c4a29f53b140789ed33e01cd3471424e50cd513c
Adrian Roed Schøning <skymon65@gmail.com>
2020-01-10 12:09:32 +0100

    LanguageManager added, for line demo of it working in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import Language.LanguageManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        LanguageManager.get().setLanguage("English");
+        System.out.println(LanguageManager.get().getString("helloWorld"));
+        LanguageManager.get().setLanguage("Dansk");
+        System.out.println(LanguageManager.get().getString("helloWorld"));
     }
 }

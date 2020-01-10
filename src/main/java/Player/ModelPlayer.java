19589adec8073b0da48c1f195e22858746a01621
Silas Rindorf <s195474@student.dtu.dk>
2020-01-10 12:57:33 +0100

    ModelPlayer made

--- src/main/java/Player/ModelPlayer.java
+++ src/main/java/Player/ModelPlayer.java
@@ -1,4 +1,26 @@
 package Player;
 
 public class ModelPlayer {
+    private String name;
+    private ModelAccount account;
+    private boolean outOfJail = false;
+    public ModelPlayer(String name){
+        this.name = name;
+        account = new ModelAccount(30000);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean isOutOfJail() {
+        return outOfJail;
+    }
+
+    public void setOutOfJail(boolean outOfJail) {
+        this.outOfJail = outOfJail;
+    }
+
+    public ModelAccount getAccount() {
+        return account;
+    }
 }

853bae816be3a32382b38ee715bd70a260dc28e6
Silas Rindorf <s195474@student.dtu.dk>
2020-01-10 13:10:09 +0100

    ModelPlayer base created

--- src/main/java/Player/ControllerPlayer.java
+++ src/main/java/Player/ControllerPlayer.java
@@ -1,4 +1,13 @@
 package Player;
 
 public class ControllerPlayer {
+    private ModelPlayer[] _playerArray;
+    public void createPlayerArray(String[] names){
+        for (int i = 0; i < names.length; i++) {
+            _playerArray[i] = new ModelPlayer(names[i]);
+        }
+    }
+    public ModelPlayer[] get_playerArray() {
+        return _playerArray;
+    }
 }

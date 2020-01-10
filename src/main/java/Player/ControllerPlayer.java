afcf64985198efa6861377e93cf4798e85802789
Silas Rindorf <s195474@student.dtu.dk>
2020-01-10 12:43:50 +0100

    ModelAccount made

---
+++ src/main/java/Player/ControllerPlayer.java
@@ -0,0 +1,4 @@
+package Player;
+
+public class ControllerPlayer {
+}
---
+++ src/main/java/Player/ModelAccount.java
@@ -0,0 +1,14 @@
+package Player;
+
+public class ModelAccount {
+    private int money;
+    public ModelAccount(int money){
+        this.money = money;
+    }
+    public int getMoney(){
+        return money;
+    }
+    public void setMoney(int money){
+        this.money = money;
+    }
+}

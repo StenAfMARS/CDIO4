8b4c9f78b430ab56abaef86e98a583225ef2566a
Silas <55868815+SilasRindorf@users.noreply.github.com>
2020-01-10 15:24:28 +0100

    ControllerGUI addPlayer to addPlayers (#26)

--- src/main/java/Game/ControllerGame.java
+++ src/main/java/Game/ControllerGame.java
@@ -1,7 +1,11 @@
 package Game;
 
+import Player.ControllerPlayer;
+
 public class ControllerGame {
     public void playGame(){
         ControllerGUI c_gui = new ControllerGUI();
+        ControllerPlayer CP = new ControllerPlayer();
+        CP.createPlayerArray(c_gui.addPlayers());
     }
 }
--- src/main/java/Player/ControllerPlayer.java
+++ src/main/java/Player/ControllerPlayer.java
@@ -2,12 +2,12 @@ package Player;
 
 public class ControllerPlayer {
     private ModelPlayer[] _playerArray;
-
     /**
      * Creates a list of players with given names
      * @param names Takes a String array to create a list of players
      */
     public void createPlayerArray(String[] names){
+        _playerArray = new ModelPlayer[names.length];
         for (int i = 0; i < names.length; i++) {
             _playerArray[i] = new ModelPlayer(names[i],30000);
         }

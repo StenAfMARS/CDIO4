7d91cd9e262f1506e28317cd5a0187d004ab52f4
Silas <55868815+SilasRindorf@users.noreply.github.com>
2020-01-12 18:55:49 +0100

    Silas/move player (#31)
    * Added movePlayer
    
    * Added Java Doc comments
    
    * Fixed all merge issues
    
    * Bug fix
    
    * Added place(house/hotel) and remove(house/hotel)
    
    * Revert "Bug fix"
    
    This reverts commit b77ab8bc1ac3ae8d7905c65ebcf74e87e2df4820.
    
    * Revert "Added place(house/hotel) and remove(house/hotel)"
    
    This reverts commit 6b3fabed2b60e6a96eb105deef6fd4446fe03a5d.
    
    * Bug fix
    
    * Added place(house/hotel) and remove(house/hotel) (#33)

--- src/main/java/Game/ControllerGUI.java
+++ src/main/java/Game/ControllerGUI.java
@@ -1,14 +1,14 @@
 package Game;
 
 import Language.LanguageManager;
-import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
+import gui_fields.GUI_Street;
 import gui_main.GUI;
 
 public class ControllerGUI {
     private GUI _gui;
     private LanguageManager _lang;
-
+    private GUI_Player[] _players;
     public ControllerGUI(){
         _gui = new GUI();
         _lang = LanguageManager.get();
@@ -40,19 +40,28 @@ public class ControllerGUI {
      * Updates all text on the board to match the current language.
      */
     public void updateBoardLanguage(){
-        for (GUI_Field gf:_gui.getFields()) {
-        }
+        /*for (GUI_Field gf:_gui.getFields()) {
+        }*/
     }
 
     /**
-     * Let's the players choose how many they are and ask for a name from each of them
-     * @return An array of player names
+     * Method for adding players on the game board and getting player names
+     * @param startBalance How much money all the players is starting with
+     * @return A string array with all the player names
      */
-    public String[] addPlayers(){
-        String[] names = new String[Integer.parseInt(_gui.getUserSelection(_lang.getString("gui.selectPlayerAmount"),"3","4","5","6"))];
+    public String[] addPlayers(int startBalance){
+        String[] names = new String[Integer.parseInt(_gui.getUserSelection(_lang.getString("gui.selectPlayerCount"),"3","4","5","6"))];
+        _players = new GUI_Player[names.length];
         for (int i = 0; i < names.length; i++) {
             names[i] = _gui.getUserString(_lang.getString("gui.selectPlayerName"));
-            _gui.addPlayer(new GUI_Player(names[i],0));
+            //No players with the same name
+            for (int j = 0; j < i; j++) {
+                if (names[i].equals(names[j])){
+                    names[i] = names[j] + " ";
+                }
+            }
+            _players[i] = new GUI_Player(names[i],startBalance);
+            _gui.addPlayer(_players[i]);
         }
         return names;
     }
@@ -68,5 +77,72 @@ public class ControllerGUI {
         }
 
         _gui.setDice(faceValue[0],faceValue[1]);
+    }
+
+    /**
+     * Function to move player the player one field at a time. Can output error on GUI
+     * @param playerID Which player to move
+     * @param currentPosition Where the player currently is placed
+     * @param newPosition The position the player has to move to
+     */
+    public void movePlayer(int playerID, int currentPosition, int newPosition){
+        for (int i = currentPosition; i < newPosition; i++) {
+            _gui.getFields()[i].setCar(_players[playerID],false);
+            _gui.getFields()[i+1].setCar(_players[playerID],true);
+            try {
+                sleep(100);
+            } catch (InterruptedException e){
+                _gui.showMessage("An error occurred");
+            }
+        }
+    }
+    public void placeHouse(int fieldID, int currentAmountOfHouses){
+        try{
+            GUI_Street street = (GUI_Street) _gui.getFields()[fieldID];
+            street.setHouses(currentAmountOfHouses + 1);
+        } catch (Exception e){
+            System.out.println("WARNING: ControllerGUI placeHouse() casting not successful. Object that got casted to street: " + _gui.getFields()[fieldID].getClass().getName());
+        }
+    }
+    public void placeHouse(int fieldID, int currentAmountOfHouses, int amountOfHousesToPlace){
+        try{
+            GUI_Street street = (GUI_Street) _gui.getFields()[fieldID];
+            street.setHouses(currentAmountOfHouses + 1);
+        } catch (Exception e){
+            System.out.println("WARNING: ControllerGUI placeHouse() casting not successful. Object that got casted to street: " + _gui.getFields()[fieldID].getClass().getName());
+        }
+    }
+    public void removeHouses(int fieldID, int currentAmountOfHouses, int amountOfHousesToRemove){
+        try{
+            GUI_Street street = (GUI_Street) _gui.getFields()[fieldID];
+            street.setHouses(currentAmountOfHouses - amountOfHousesToRemove);
+        } catch (Exception e){
+            System.out.println("WARNING: ControllerGUI removeHouse() casting not successful. Object that got casted to street: " + _gui.getFields()[fieldID].getClass().getName());
+        }
+    }
+    public void placeHotel(int fieldID){
+        try{
+            GUI_Street street = (GUI_Street) _gui.getFields()[fieldID];
+            street.setHotel(true);
+        } catch (Exception e){
+            System.out.println("WARNING: ControllerGUI placehotel() casting not successful. Object that got casted to street: " + _gui.getFields()[fieldID].getClass().getName());
+        }
+    }
+    public void removeHotel(int fieldID){
+        try{
+            GUI_Street street = (GUI_Street) _gui.getFields()[fieldID];
+            street.setHotel(false);
+        } catch (Exception e){
+            System.out.println("WARNING: ControllerGUI removeHotel() casting not successful. Object that got casted to street: " + _gui.getFields()[fieldID].getClass().getName());
+        }
+    }
+
+    /**
+     * Private sleep function
+     * @param milliseconds How many milliseconds to sleep for
+     * @throws InterruptedException Throws exception from Thread.sleep
+     */
+    private void sleep(long milliseconds) throws InterruptedException {
+        Thread.sleep(milliseconds);
     }
 }
--- src/main/java/Game/ControllerGame.java
+++ src/main/java/Game/ControllerGame.java
@@ -4,12 +4,12 @@ import Fields.ControllerField;
 import Player.ControllerPlayer;
 
 public class ControllerGame {
-    public void playGame(){
+    public void playGame() {
         ControllerGUI c_gui = new ControllerGUI();
         ControllerPlayer c_player = new ControllerPlayer();
         ControllerField c_field = new ControllerField();
 
         c_gui.changeBoardLanguage();
-        c_player.createPlayerArray(c_gui.addPlayers());
+        c_player.createPlayerArray(c_gui.addPlayers(0));
     }
 }

32cc353e470222b4902475df5873fcd081de4f2f
Silas Rindorf <s195474@student.dtu.dk>
2020-01-12 15:00:42 +0100

    Added movePlayer

--- src/main/java/Game/ControllerGUI.java
+++ src/main/java/Game/ControllerGUI.java
@@ -1,33 +1,67 @@
 package Game;
 
 import Language.LanguageManager;
-import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
 import gui_main.GUI;
 
 public class ControllerGUI {
     private GUI _gui;
-
+    private LanguageManager _lang = LanguageManager.get();
+    private GUI_Player[] _players;
     public ControllerGUI(){
         _gui = new GUI();
     }
 
     public void changeBoardLanguage(String newLanguage){
-        LanguageManager.get().setLanguage(newLanguage);
-        for (GUI_Field gf:_gui.getFields()) {
+        _lang.setLanguage(newLanguage);
+        for (int i = 0; i < _gui.getFields().length; i++) {
+            //Start field
+            if (i == 0){
+            }
+            //Chance field
+            else if(i == 2 || i == 7 || i == 17 || i == 22 || i == 33 || i == 36){
+            }
+            //Park or Visit Jail
+            else if (i == 10 || i == 20){
+
+            }
+            //Go to jail
+            else if (i == 30){
+
+            }
         }
     }
+    public void changeBoardLanguage(){
+        _lang.setLanguage(_gui.getUserSelection(_lang.getString("selectLanguage"),_lang.getLanguages()));
+    }
 
-    public String[] addPlayers(){
-        String[] names = new String[Integer.parseInt(_gui.getUserSelection("Select amount of players","3","4","5","6"))];
+    public String[] addPlayers(int startBalance){
+        String[] names = new String[Integer.parseInt(_gui.getUserSelection(_lang.getString("selectPlayerCount"),"3","4","5","6"))];
+        _players = new GUI_Player[names.length];
         for (int i = 0; i < names.length; i++) {
-            names[i] = _gui.getUserString("Select player");
-            _gui.addPlayer(new GUI_Player(names[i],0));
+            names[i] = _gui.getUserString(_lang.getString("selectPlayerName"));
+            _players[i] = new GUI_Player(names[i],startBalance);
+            _gui.addPlayer(_players[i]);
         }
         return names;
     }
 
-    public void displayDieOnBoard(int faceValue[]){
+    public void displayDieOnBoard(int[] faceValue){
         _gui.setDice(faceValue[0],faceValue[1]);
+    }
+
+    public void movePlayer(int playerID, int currentPosition, int newPosition){
+        for (int i = currentPosition; i < newPosition; i++) {
+            _gui.getFields()[i].setCar(_players[playerID],false);
+            _gui.getFields()[i+1].setCar(_players[playerID],true);
+            try {
+                sleep(100);
+            } catch (InterruptedException e){
+                _gui.showMessage("An error occurred");
+            }
+        }
+    }
+    private void sleep(long miliseconds) throws InterruptedException {
+        Thread.sleep(miliseconds);
     }
 }
--- src/main/java/Game/ControllerGame.java
+++ src/main/java/Game/ControllerGame.java
@@ -3,9 +3,10 @@ package Game;
 import Player.ControllerPlayer;
 
 public class ControllerGame {
-    public void playGame(){
+    public void playGame() {
         ControllerGUI c_gui = new ControllerGUI();
-        ControllerPlayer CP = new ControllerPlayer();
-        CP.createPlayerArray(c_gui.addPlayers());
+        ControllerPlayer c_player = new ControllerPlayer();
+        c_gui.changeBoardLanguage();
+        c_player.createPlayerArray(c_gui.addPlayers(0));
     }
 }

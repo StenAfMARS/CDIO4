058dc0cae60c5a82820a753905d00198286684fd
Mathias Bærentzen <mathias.baerentzen@gmail.com>
2020-01-13 13:23:17 +0100

    created ModelMoveTo

---
+++ src/main/java/Chancecard/ModelMoveTo.java
@@ -0,0 +1,12 @@
+package Chancecard;
+
+public class ModelMoveTo {
+    private int[] _destination;
+    public ModelMoveTo(int[] desitnation){
+        _destination = desitnation;
+    }
+
+    public int[] get_destination() {
+        return _destination;
+    }
+}

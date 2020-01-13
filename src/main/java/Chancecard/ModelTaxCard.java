7879c68e87c89d5e52c54f375fac4bd20c6f7af2
mathiasBaerentzen <mathias.baerentzen@gmail.com>
2020-01-13 13:26:30 +0100

    Mathias/chance card controller (#37)
    * setup CCD_Controller and created danish CC Deck
    
    * created english cards
    
    * chanced
    
    * created ModelTaxCard
    
    * created ModelChangeMoneyCard
    
    * created ModelMoveCard
    
    * created ModelMoveTo

---
+++ src/main/java/Chancecard/ModelTaxCard.java
@@ -0,0 +1,14 @@
+package Chancecard;
+
+public class ModelTaxCard {
+    private int _pricePrHouse = 500;
+    private int _pricePrHotel = 1000;
+
+    public int getPricePrHouse() {
+        return _pricePrHouse;
+    }
+
+    public int getPricePrHotel() {
+        return _pricePrHotel;
+    }
+}

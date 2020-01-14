4c57b96934d7bccd2a91832cd31a40737013608b
Adrian Roed Schøning <skymon65@gmail.com>
2020-01-14 10:48:40 +0100

    Auction up and working

--- src/main/java/Fields/ModelProperty.java
+++ src/main/java/Fields/ModelProperty.java
@@ -3,6 +3,7 @@ package Fields;
 public abstract class ModelProperty extends ModelField {
     private int _propertyPrice;
     private int _mortgage;
+    private int _owner;
 
     public int get_propertyPrice() {
         return _propertyPrice;
@@ -22,4 +23,11 @@ public abstract class ModelProperty extends ModelField {
      * @return The rent to be paid when landing on the property.
      */
     public abstract int get_rent();
+
+    public int get_owner() {
+        return _owner;
+    }
+    public void set_owner(int _owner) {
+        this._owner = _owner;
+    }
 }

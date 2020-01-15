07241929588640eb7fb1f2faee8cbd66d4cd526a
s195481 <55875270+s195481@users.noreply.github.com>
2020-01-15 10:27:43 +0100

    Sejr/bug fixes (#79)
    * bug fixes to:
    - ControllerGame
    - LanguageManager
    - Language Properties
    
    * Changed colors for cars to random colors.
    
    * Edited prices and rent for fields.

--- src/main/java/Fields/ControllerField.java
+++ src/main/java/Fields/ControllerField.java
@@ -35,37 +35,37 @@ public class ControllerField {
         _fields[6] = new ModelEstate("roskildevej",Color.PINK,2000,new int[]{100,600,1800,5400,8000,11000}, 1000);
         _fields[7] = new ModelChanceField("prøvLykken", null);
         _fields[8] = new ModelEstate("valbyLanggade",Color.PINK,2000,new int[]{100,600,1800,5400,8000,11000}, 1000);
-        _fields[9] = new ModelEstate("allegade",Color.PINK,2000,new int[]{100,600,1800,5400,8000,11000}, 1000);
+        _fields[9] = new ModelEstate("allegade",Color.PINK,2400,new int[]{150,800,2000,6000,9000,12000}, 1000);
         _fields[10] = new ModelVisitJail("fængsel",null);
         _fields[11] = new ModelEstate("frederiksbergsAlle",Color.GREEN,2800,new int[]{200,1000,3000,9000,12500,15000}, 2000);
         _fields[12] = new ModelBrewery("tuborg",new Color(152,251,152),3000,100);
         _fields[13] = new ModelEstate("bulowsvej",Color.GREEN,2800,new int[]{200,1000,3000,9000,12500,15000}, 2000);
-        _fields[14] = new ModelEstate("gammelKongevej",Color.GREEN,2800,new int[]{200,1000,3000,9000,12500,15000}, 2000);
+        _fields[14] = new ModelEstate("gammelKongevej",Color.GREEN,3200,new int[]{250,1250,3750,10000,14000,18000}, 2000);
         _fields[15] = new ModelFerry("dFDS",Color.ORANGE,4000,500);
         _fields[16] = new ModelEstate("bernstorffsvej", Color.GRAY, 3600,new int[]{300,1400,4000,11000,15000,19000},2000);
         _fields[17] = new ModelChanceField("prøvLykken", null);
         _fields[18] = new ModelEstate("hellerupvej", Color.GRAY, 3600,new int[]{300,1400,4000,11000,15000,19000},2000);
-        _fields[19] = new ModelEstate("strandvejen", Color.GRAY, 3600,new int[]{300,1400,4000,11000,15000,19000},2000);
+        _fields[19] = new ModelEstate("strandvejen", Color.GRAY, 4000,new int[]{350,1600,4400,12000,16000,20000},2000);
         _fields[20] = new ModelParking("helle",null);
         _fields[21] = new ModelEstate("trianglen",Color.RED,4400,new int[]{350,1800,5000,14000,17500,21000},3000);
         _fields[22] = new ModelChanceField("prøvLykken",null);
         _fields[23] = new ModelEstate("østerbrogade",Color.RED,4400,new int[]{350,1800,5000,14000,17500,21000},3000);
-        _fields[24] = new ModelEstate("grønningen",Color.RED,4400,new int[]{350,1800,5000,14000,17500,21000},3000);
+        _fields[24] = new ModelEstate("grønningen",Color.RED,4800,new int[]{400,2000,6000,15000,18500,22000},3000);
         _fields[25] = new ModelFerry("øS",Color.ORANGE,4000,500);
         _fields[26] = new ModelEstate("bredgade",Color.CYAN,5200,new int[]{450,2200,6600,16000,19500,23000},3000);
         _fields[27] = new ModelEstate("kgsNytorv",Color.CYAN,5200,new int[]{450,2200,6600,16000,19500,23000},3000);
         _fields[28] = new ModelBrewery("carlsberg", new Color(152,251,152),3000,100);
-        _fields[29] = new ModelEstate("østergade",Color.CYAN,5200,new int[]{450,2200,6600,16000,19500,23000},3000);
+        _fields[29] = new ModelEstate("østergade",Color.CYAN,5600,new int[]{500,2400,7200,17000,20500,24000},3000);
         _fields[30] = new ModelGotoJail("gåIFængsel",null);
         _fields[31] = new ModelEstate("amagertorv",Color.YELLOW,6000,new int[]{550,2600,7800,18000,22000,25000}, 4000);
         _fields[32] = new ModelEstate("vimmelskaftet",Color.YELLOW,6000,new int[]{550,2600,7800,18000,22000,25000}, 4000);
         _fields[33] = new ModelChanceField("prøvLykken",null);
-        _fields[34] = new ModelEstate("nygade",Color.YELLOW,6000,new int[]{550,2600,7800,18000,22000,25000}, 4000);
+        _fields[34] = new ModelEstate("nygade",Color.YELLOW,6400,new int[]{600,3000,9000,20000,24000,28000}, 4000);
         _fields[35] = new ModelFerry("bornholm",Color.ORANGE,4000,500);
         _fields[36] = new ModelChanceField("prøvLykken",null);
         _fields[37] = new ModelEstate("frederiksberggade",Color.MAGENTA,7000,new int[]{700,3500,10000,22000,26000,30000},4000);
         _fields[38] = new ModelIncomeTax("ekstraordinærStatsskat",null);
-        _fields[39] = new ModelEstate("rådhuspladsen",Color.MAGENTA,7000,new int[]{700,3500,10000,22000,26000,30000},4000);
+        _fields[39] = new ModelEstate("rådhuspladsen",Color.MAGENTA,8000,new int[]{1000,4000,12000,28000,34000,40000},4000);
     }
 
     public int getFieldType(int fieldID){

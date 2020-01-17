3940668674fff0ff702b4fac30d17924ca6d7e55
s195481 <s195481@student.dtu.dk>
2020-01-17 08:26:44 +0100

    Added code commentaries to ModelVisitJail.

--- src/main/java/Fields/ModelVisitJail.java
+++ src/main/java/Fields/ModelVisitJail.java
@@ -3,6 +3,11 @@ package Fields;
 import java.awt.*;
 
 class ModelVisitJail extends ModelField {
+ /**
+  * ModelVisitJail used to make a model for the field Visit Jail.
+  * @param name name of the field.
+  * @param backgroundColor background color of the field.
+  */
  public ModelVisitJail(String name, Color backgroundColor) {
   super(name, backgroundColor);
  }

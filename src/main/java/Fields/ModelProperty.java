fedff7820fa857da40a42f243dfad3c80c25bca4
s195481 <s195481@student.dtu.dk>
2020-01-17 08:25:21 +0100

    Added code commentaries to ModelProperty.

--- src/main/java/Fields/ModelProperty.java
+++ src/main/java/Fields/ModelProperty.java
@@ -7,6 +7,13 @@ abstract class ModelProperty extends ModelField {
     private int _mortgage;
     private int _owner;
 
+    /**
+     * ModelProperty used to make a model for a property field.
+     * @param name name of the field.
+     * @param backgroundColor background color of a field.
+     * @param propertyPrice property price for a field.
+     * @param mortgage mortgage of a field.
+     */
     public ModelProperty(String name, Color backgroundColor,int propertyPrice, int mortgage) {
         super(name,backgroundColor);
         this._propertyPrice = propertyPrice;
@@ -14,6 +21,10 @@ abstract class ModelProperty extends ModelField {
         this._owner = -1;
     }
 
+    /**
+     * get_propertyPrice() used to get the price of a property.
+     * @return private attribute _propertyPrice
+     */
     int get_propertyPrice() {
         return _propertyPrice;
     }
@@ -29,16 +40,31 @@ abstract class ModelProperty extends ModelField {
     }
 
     /**
+     * get_rent() used to
      * @return The rent to be paid when landing on the property.
      */
      abstract int get_rent();
 
+    /**
+     * get_owner() used to
+     * @return _owner of a field.
+     */
      int get_owner() {
         return _owner;
     }
+
+    /**
+     * set_owner() used to set owner of a field.
+     * @param _owner
+     */
      void set_owner(int _owner) {
         this._owner = _owner;
     }
+
+    /**
+     * isOwned() used to check if a field is owned.
+     * @return private attribute _owner.
+     */
     boolean isOwned(){
          return _owner != -1;
     }

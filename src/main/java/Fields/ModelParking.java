968cfba3504d4d602421ce7234ae939f52fa2370
s195481 <s195481@student.dtu.dk>
2020-01-17 08:21:20 +0100

    Added code commentaries to ModelParking.

--- src/main/java/Fields/ModelParking.java
+++ src/main/java/Fields/ModelParking.java
@@ -3,6 +3,11 @@ package Fields;
 import java.awt.*;
 
 class ModelParking extends ModelField {
+ /**
+  * ModelParking used to make a model for the parking field.
+  * @param name name of the field.
+  * @param backgroundColor background color of the field.
+  */
  public ModelParking(String name, Color backgroundColor) {
   super(name, backgroundColor);
  }

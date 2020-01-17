ca2fcee25dd0fca67c09e9790c57b0f60ca4f840
s195481 <s195481@student.dtu.dk>
2020-01-17 08:07:54 +0100

    Added code commentaries to ModelChanceField.

--- src/main/java/Fields/ModelChanceField.java
+++ src/main/java/Fields/ModelChanceField.java
@@ -3,6 +3,10 @@ package Fields;
 import java.awt.*;
 
 class ModelChanceField extends ModelField {
+ /**
+  * @param name name of the field type.
+  * @param backgroundColor background color for the field.
+  */
  public ModelChanceField(String name, Color backgroundColor) {
   super(name, backgroundColor);
  }

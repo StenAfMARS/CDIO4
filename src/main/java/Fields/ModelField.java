353ac2d7772f961e30b3cb44b1232a0a58e0fd53
s195481 <s195481@student.dtu.dk>
2020-01-17 08:17:36 +0100

    Added code commentaries to ModelField.

--- src/main/java/Fields/ModelField.java
+++ src/main/java/Fields/ModelField.java
@@ -6,6 +6,11 @@ import java.awt.*;
     private String _name;
     private Color _backgroundColor;
 
+     /**
+      * ModelField used to find the name and backgroundColor for a given field.
+      * @param name the name of the field.
+      * @param backgroundColor the background color of the field.
+      */
     protected ModelField(String name, Color backgroundColor){
         this._name = name;
         this._backgroundColor = backgroundColor;
@@ -18,6 +23,10 @@ import java.awt.*;
         this._backgroundColor = _backgroundColor;
     }
 
+     /**
+      * get_name() used to
+      * @return private attribute _name.
+      */
      public String get_name() {
          return _name;
      }

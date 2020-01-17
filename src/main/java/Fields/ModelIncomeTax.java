8484ede72e46bfd20ee244935ab7636e4781b207
s195481 <s195481@student.dtu.dk>
2020-01-17 08:20:47 +0100

    Added code commentaries to ModelIncomeTax.

--- src/main/java/Fields/ModelIncomeTax.java
+++ src/main/java/Fields/ModelIncomeTax.java
@@ -4,6 +4,12 @@ import java.awt.*;
 class ModelIncomeTax extends ModelField {
  private int _rent;
 
+ /**
+  * ModelIncomeTax used to make a model for the income tax field.
+  * @param name name of the field.
+  * @param backgroundColor background color of the field.
+  * @param rent rent of the field.
+  */
  public ModelIncomeTax(String name, Color backgroundColor, int rent) {
   super(name, backgroundColor);
   this._rent = rent;

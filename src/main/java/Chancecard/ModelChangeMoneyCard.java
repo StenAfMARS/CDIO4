2b400cb7ba9ffd3a2c6bb0f07a7ac074bcc17d55
s195481 <s195481@student.dtu.dk>
2020-01-17 09:00:28 +0100

    Added code commentaries to ModelChangeMoneyCard.

--- src/main/java/Chancecard/ModelChangeMoneyCard.java
+++ src/main/java/Chancecard/ModelChangeMoneyCard.java
@@ -2,10 +2,19 @@ package Chancecard;
 
  class ModelChangeMoneyCard extends ModelChanceCard{
     private int _amount;
+
+     /**
+      * ModelChangeMoneyCard() used to make a model for the ChangeMoneyCard.
+      * @param amount - amount of money.
+      */
      ModelChangeMoneyCard(int amount){
         _amount = amount;
     }
 
+     /**
+      * get_amount(9
+      * @return private attribut _amount. Amount of money.
+      */
      int get_amount() {
         return _amount;
     }

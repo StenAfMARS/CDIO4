1b1020d6e5cae3f885285dfeea0bfb4306f9f0d9
s195481 <s195481@student.dtu.dk>
2020-01-17 08:19:00 +0100

    Added code commentaries to ModelGoToJail.

--- src/main/java/Fields/ModelGotoJail.java
+++ src/main/java/Fields/ModelGotoJail.java
@@ -3,6 +3,11 @@ package Fields;
 import java.awt.*;
 
 class ModelGotoJail extends ModelField {
+ /**
+  * ModelGoToJail used to model the field "Go to jail".
+  * @param name the name of the field.
+  * @param backgroundColor the background color of the field.
+  */
  public ModelGotoJail(String name, Color backgroundColor) {
   super(name, backgroundColor);
  }

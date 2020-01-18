97e25b81eecf8bba3aa43692d068d65970e13f30
Sejr <sejrlba@gmail.com>
2020-01-18 15:44:24 +0100

    Added java doc to ModelChanceCard.

--- src/main/java/Chancecard/ModelChanceCard.java
+++ src/main/java/Chancecard/ModelChanceCard.java
@@ -3,10 +3,18 @@ package Chancecard;
  abstract class ModelChanceCard {
   private int _iD;
 
+  /**
+   * ModelChanceCard() used for making a model of a chance card.
+   * @param iD chance card ID
+   */
   ModelChanceCard(int iD) {
    this._iD = iD;
   }
 
+  /**
+   * get_iD() used for getting the chance card ID.
+   * @return private attribute _iD. The ID of a chance card.
+   */
   public int get_iD() {
    return _iD;
   }

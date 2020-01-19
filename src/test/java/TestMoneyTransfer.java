17d32f47cc262d6a96ce04b2060cc65780567a91
Mathias Bærentzen <mathias.baerentzen@gmail.com>
2020-01-19 22:17:09 +0100

    lavet test case transferMoneyFromPlayerToPlayer

--- src/test/java/TestMoneyTransfer.java
+++ src/test/java/TestMoneyTransfer.java
@@ -11,12 +11,28 @@ public class TestMoneyTransfer {
         ControllerPlayer c_player = new ControllerPlayer();
         ControllerGUI c_gui = ControllerGUI.get();
         c_player.createPlayerArray(c_gui.addPlayers(30000));
-        for(int i =0;i<1000;i++){
+        for(int j = 0;j <= 3 ;j++){
+            for(int i =0;i<1000;i++){
+                int testAmount = (int)(Math.random()*1000);
+                c_player.changePlayerMoney(testAmount,j);
+
+                assertTrue(c_player.get_account(0)>=30000);
+            }
+        }
+
+    }
+    @Test
+    public void transferMoneyFromPlayerToPlayer() {
+        ControllerPlayer c_player = new ControllerPlayer();
+        ControllerGUI c_gui = ControllerGUI.get();
+        c_player.createPlayerArray(c_gui.addPlayers(30000));
+        for(int i =0;i<1;i++){
             int testAmount = (int)(Math.random()*1000);
             c_player.changePlayerMoney(testAmount,0);
-
-            assertTrue(c_player.get_account(0)>=30000);
+            c_player.changePlayerMoney(-testAmount,1);
+            assertTrue(c_player.get_account(0)>=30000 || c_player.get_account(0)<=30000);
         }
     }
+
 
 }

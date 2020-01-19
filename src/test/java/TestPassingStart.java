418aa9a221295c1cb22e941baa9995eeaaeb1d14
Peter Skaarup <peterskaarup@gmail.com>
2020-01-19 21:51:04 +0100

    TestPassingStart

--- src/test/java/TestPassingStart.java
+++ src/test/java/TestPassingStart.java
@@ -1,8 +1,21 @@
+import Player.ControllerPlayer;
+import Player.ModelAccount;
+import Player.ModelPlayer;
 import org.junit.Test;
+
+import java.lang.reflect.Array;
 
 public class TestPassingStart {
    @Test
     public void shouldGet4000WhenPassingStart(){
+       String[] pNames = {"FørsteFisk"};
+       ControllerPlayer c_player = new ControllerPlayer();
+       ModelAccount m_account = new ModelAccount(0);
+
+       c_player.createPlayerArray(pNames);
+       c_player.setPlayerPosition(0,1);
+       c_player.changePlayerPosition(0,10);
+
 
    }
    

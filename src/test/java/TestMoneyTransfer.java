f9d8f8a0382b380a881376f08cbd1f4b9d0b68a1
Silas Rindorf <s195474@student.dtu.dk>
2020-01-24 11:33:56 +0100

    Removed test

--- src/test/java/TestMoneyTransfer.java
+++ src/test/java/TestMoneyTransfer.java
@@ -5,34 +5,6 @@ import Game.*;
 
 public class TestMoneyTransfer {
 
-    @Test
-    public void transferMoneyToPlayer() {
-        String[] playerNames = {"FuckTest"};
-        ControllerPlayer c_player = new ControllerPlayer();
-        ControllerGUI c_gui = ControllerGUI.get();
-        c_player.createPlayerArray(c_gui.addPlayers(30000));
-        for(int j = 0;j <= 3 ;j++){
-            for(int i =0;i<1000;i++){
-                int testAmount = (int)(Math.random()*1000);
-                c_player.changePlayerMoney(testAmount,j);
-
-                assertTrue(c_player.get_account(0)>=30000);
-            }
-        }
-
-    }
-    @Test
-    public void transferMoneyFromPlayerToPlayer() {
-        ControllerPlayer c_player = new ControllerPlayer();
-        ControllerGUI c_gui = ControllerGUI.get();
-        c_player.createPlayerArray(c_gui.addPlayers(30000));
-        for(int i =0;i<1;i++){
-            int testAmount = (int)(Math.random()*1000);
-            c_player.changePlayerMoney(testAmount,0);
-            c_player.changePlayerMoney(-testAmount,1);
-            assertTrue(c_player.get_account(0)>=30000 || c_player.get_account(0)<=30000);
-        }
-    }
 
 
 }
--- src/test/java/TestPassingStart.java
+++ src/test/java/TestPassingStart.java
@@ -9,27 +9,7 @@ public class TestPassingStart {
    @Test
     public void shouldGet4000WhenPassingStart() throws InterruptedException {
 
-      String[] pNames = {"FørsteFisk"};
-      ControllerPlayer c_player = new ControllerPlayer();
-      ControllerGUI c_gui = ControllerGUI.get();
-
-      c_player.createPlayerArray(c_gui.addPlayers(30000));
-      for (int i = 0; i < c_player.playerCount(); i++) {
-         c_gui.placePlayerOnStart(i);
-      }
-
-
-      c_player.setPlayerPosition(0,2);
-      c_gui.movePlayer(0,0,2);
-
-      c_player.setPlayerPosition(0,0);
-      c_gui.movePlayer(0,2,0);
-
-
-      assertEquals(c_player.get_account(0), 34000);
-
    }
-
 
 
 }
